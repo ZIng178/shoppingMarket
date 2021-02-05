f37f6b379ef9be3931909407303e4b03f84f26d9
unknown <wangdi457@gmail.com>
2021-02-04 19:15:37 -0500

    Initalized npm added express server created an endpoint to listen and get a message

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+
+env.config();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is lstening on port ${process.env.PORT}`);
+});
+
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Hello from server ",
+  });
+});
